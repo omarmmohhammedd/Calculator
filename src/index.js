c2f766aaacf5cadea828ba070d65fe6a888cb0b2
omar258 <omar.mmohhammedd@gmail.com>
2022-08-12 22:47:16 +0200

    Final Edition

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router } from "react-router-dom";
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
